2f4bae073ca40e4614b3df8a07856d00d8977167
Manindhra Goud <manindhra1412@gmail.com>
2024-10-30 15:25:52 +0530

    c7

--- index.test.js
+++ index.test.js
@@ -1,5 +1,4 @@
-// index.test.js
-const sum = require('./index');
+const sum = require('./index'); // Correct path for index.js in the same directory
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
